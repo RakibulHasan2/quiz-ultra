b7b49fb9300e28d3132cac64c9fc7594e5c52ec5
RakibulHasan2 <rakibulhasan99445@gmail.com>
2022-10-11 10:19:15 +0600

    quizzes responsive done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
-        <nav className='header'>
-            <h1 className='header-name'>Quiz-Ultra</h1>
-            <div>
+        <nav className='flex justify-around items-center h-20 background-header '>
+            <h1 className='text-3xl text-white font-bold'>Quiz-Ultra</h1>
+            <div className='text-white text-xl'>
                 <Link to='/'>Quizzes</Link>
-                <Link to='result'>Result</Link>
-                <Link to='blog'>Blog</Link>
+                <Link to='result' className='ml-4'>Result</Link>
+                <Link to='blog' className='ml-4'>Blog</Link>
             </div>
         </nav>
     );
--- src/components/Quizzes/Quizzes.js
+++ src/components/Quizzes/Quizzes.js
@@ -8,8 +8,10 @@ const Quizzes = () => {
     // console.log(quizzes)
     return (
         <div>
-            <h1 className='quiz-heading'>We have made a fun and informative quiz and game for students. <br /> Hope You will enjoy this !!</h1>
-            <div  className='all-quiz'>
+            <div className='bg-slate-200 w-11/12 ml-14 h-40 background'>
+            <h1 className='quiz-heading text-xl font-bold pt-10'>We have made a fun and informative quiz and game for students. <br /> Hope You will enjoy this !!</h1>
+            </div>
+            <div  className='flex justify-center gap-14'>
             {
                 quizzes.map(quiz => <Quiz
                 key={quiz.id}
